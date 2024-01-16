9b954e42f5dc66670f526d10df5a5ca2c73af93f
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-01-16 22:20:47 +0600

    dynamic task list and react icons added

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavourite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -12,7 +26,7 @@ export default function TaskBoard() {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
