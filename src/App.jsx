9caf4997a9f7b24864a85934f0ac3c935d9274f8
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-01-16 21:37:54 +0600

    template splitting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 function App() {
   return (
     <>
-      <p>welcome to tasker</p>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
     </>
   );
 }
