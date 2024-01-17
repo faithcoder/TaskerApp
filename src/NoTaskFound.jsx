0255a779604aedbc749e0e72adc59b6c9949c87f
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-01-17 13:18:45 +0600

    no task found updated

---
+++ src/NoTaskFound.jsx
@@ -0,0 +1,3 @@
+export default function NoTaskFound() {
+  return <p className="text-center">No Task Found, please Add One</p>;
+}
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import NoTaskFound from "../NoTaskFound";
 import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskAction";
@@ -59,6 +60,25 @@ export default function TaskBoard() {
     setTasks([...tasks]);
   }
 
+  function handleFavourite(taskId) {
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+    const newTasks = [...tasks];
+
+    newTasks[taskIndex].isFavourite = !newTasks[taskIndex].isFavourite;
+    setTasks(newTasks);
+  }
+
+  function handleSearch(searchTerm) {
+    console.log(searchTerm);
+
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setTasks([...filtered]);
+  }
+
   return (
     <section className="mb-20" id="tasks">
       {showAddModal && (
@@ -70,7 +90,7 @@ export default function TaskBoard() {
       )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask />
+          <SearchTask onSearch={handleSearch} />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
@@ -78,11 +98,16 @@ export default function TaskBoard() {
             onAddClick={() => setShowAddModal(true)}
             onDeleteAllClick={handleDeleteAllClick}
           />
-          <TaskList
-            tasks={tasks}
-            onEdit={handleEditTask}
-            onDelete={handleDeleteTask}
-          />
+          {tasks.length > 0 ? (
+            <TaskList
+              tasks={tasks}
+              onEdit={handleEditTask}
+              onDelete={handleDeleteTask}
+              onFav={handleFavourite}
+            />
+          ) : (
+            <NoTaskFound />
+          )}
         </div>
       </div>
     </section>
